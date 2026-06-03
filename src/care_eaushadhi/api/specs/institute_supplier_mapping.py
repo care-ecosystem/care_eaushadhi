a8f37c14344d44aaac4042bb45e4181157cd6e7e
Mahawar_Khushi-egov <khushi.mahawar@egovernments.org>
2026-06-03 17:38:51 +0530

    feat: exposed meta and supplier_name in response body (#52)
    Co-authored-by: mahawarkhushi <mahawarkhushi27@gmail.com>
    Co-authored-by: Jagankumar E <53823168+jagankumar-egov@users.noreply.github.com>

--- src/care_eaushadhi/api/specs/institute_supplier_mapping.py
+++ src/care_eaushadhi/api/specs/institute_supplier_mapping.py
@@ -1,17 +1,14 @@
 import datetime
 from pydantic import UUID4
-
 from care.emr.resources.base import EMRResource
-
 from care_eaushadhi.models.eaushadhi_institute_supplier_mapping import EAushadhiInstituteSupplierMapping
-
 
 class InstituteSupplierMappingReadSpec(EMRResource):
     __model__ = EAushadhiInstituteSupplierMapping
     __exclude__ = []
-
     id: UUID4 | None = None
     supplier_id: UUID4 | None = None
+    supplier_name: str | None = None
     eaushadhi_warehouse_name: str | None = None
     is_default: bool = False
     created_by: dict | None = None
@@ -23,4 +20,5 @@ class InstituteSupplierMappingReadSpec(EMRResource):
     def perform_extra_serialization(cls, mapping, obj):
         mapping["id"] = obj.external_id
         mapping["supplier_id"] = obj.supplier.external_id if obj.supplier else None
+        mapping["supplier_name"] = obj.supplier.name if obj.supplier else None
         cls.serialize_audit_users(mapping, obj)
