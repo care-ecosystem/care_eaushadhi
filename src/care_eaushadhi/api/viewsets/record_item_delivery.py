e481c28cf854f42b82a374e9de14e79a7524bd04
Muhammed Sawad <muhammed.sawad@egov.global>
2026-06-25 14:24:07 +0530

    Added logic to create/update data in product mappings (#116)
    * Added logic to record data in product mappings
    
    * Apply suggestions from code review
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jagankumar E <53823168+jagankumar-egov@users.noreply.github.com>
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- src/care_eaushadhi/api/viewsets/record_item_delivery.py
+++ src/care_eaushadhi/api/viewsets/record_item_delivery.py
@@ -1,5 +1,5 @@
 from django.db import IntegrityError
-
+from django.utils import timezone
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.exceptions import (
@@ -25,6 +25,9 @@ from care_eaushadhi.models.eaushadhi_inward_record_item_delivery import (
     EAushadhiInwardRecordItemDelivery,
     InwardRecordItemDeliveryStatus,
 )
+from care_eaushadhi.models.eaushadhi_product_mapping import (
+    EAushadhiProductMapping,
+)
 
 
 class ConflictException(APIException):
@@ -38,8 +41,6 @@ class RecordItemDeliveryViewSet(
     EMRUpdateMixin,
     EMRBaseViewSet,
 ):
-
-    http_method_names = ["post", "patch"]
 
     database_model = EAushadhiInwardRecordItemDelivery
 
@@ -58,16 +59,44 @@ class RecordItemDeliveryViewSet(
             )
 
     def perform_create(self, instance):
+
         self._authorize_facility(instance.facility)
         instance.created_by = self.request.user
         instance.updated_by = self.request.user
 
-
         try:
             instance.save()
+            self._sync_product_mapping(instance)
         except IntegrityError as exc:
-
             raise
+
+    def _sync_product_mapping(self, delivery_item):
+
+        facility = delivery_item.facility
+        eaushadhi_drug_id = delivery_item.inward_record_item.drug_id
+        product_knowledge = delivery_item.product_knowledge
+        eaushadhi_drug_name = delivery_item.inward_record_item.drug_name
+ 
+        mapping, created = EAushadhiProductMapping.objects.get_or_create(
+            facility=facility,
+            eaushadhi_drug_id=eaushadhi_drug_id,
+            product_knowledge=product_knowledge,
+            defaults={
+                'eaushadhi_drug_name': eaushadhi_drug_name,
+                'usage_count': 1,
+                'last_used_date': timezone.now(),
+                'created_by': self.request.user,
+                'updated_by': self.request.user,
+            }
+        )
+ 
+        if not created:
+            from django.db.models import F
+            EAushadhiProductMapping.objects.filter(pk=mapping.pk).update(
+                usage_count=F('usage_count') + 1,
+                last_used_date=timezone.now(),
+                updated_by=self.request.user,
+            )
 
     def clean_update_data(self, request_data, keep_fields: set | None = None):
         clean_data = super().clean_update_data(request_data, keep_fields)
@@ -98,9 +127,10 @@ class RecordItemDeliveryViewSet(
                 )
 
     def partial_update(self, request, *args, **kwargs):
+
         instance = self.get_object()
         return Response(self.handle_update(instance, request.data))
-
+    
     def authorize_update(self, request_obj, model_instance):
         self._authorize_facility(model_instance.facility)
 
